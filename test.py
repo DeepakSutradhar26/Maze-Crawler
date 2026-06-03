01cbfaae3ef8fee5d6e4c1ed3b3117788b5fae74
Deepak Sutradhar <140784188+DeepakSutradhar26@users.noreply.github.com>
2026-06-03 13:39:42 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+from kaggle_environments import make
+
+env = make("crawl", configuration={"randomSeed": 42}, debug=True)
+env.run(["main.py", "random"])
+
+# View result
+final = env.steps[-1]
+for i, s in enumerate(final):
+    print(f"Player {i}: reward={s.reward}, status={s.status}")
+
+# Render in a notebook
+env.render(mode="ipython", width=800, height=800)
